8c855a8b91322d8e2f34f0369488a254d3c137cd
Andrescpyo <142014854+Andrescpyo@users.noreply.github.com>
2023-09-17 22:39:05 -0500

    version 3.1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.init()
 # Configuración de la pantalla
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Triángulo Móvil')
+pygame.display.set_caption('Triángulo Sobre Hielo')
 
 # Colores
 black = (0, 0, 0)
@@ -23,10 +23,14 @@ triangle_speed = 5
 triangle_angle = 0
 triangle_rotation_speed = 5
 
-# Variables de inercia
-inertia = 0.95
-inertia_speed_x = 0
-inertia_speed_y = 0
+# Variables de fricción
+friction = 0.98 # Fricción normal (menos deslizamiento)
+slippery_friction = 0.6  # Fricción reducida para simular hielo (más deslizamiento)
+current_friction = friction
+
+# Velocidades
+triangle_speed_x = 0
+triangle_speed_y = 0
 
 # Bucle principal
 running = True
@@ -40,15 +44,11 @@ while running:
 
     # Movimiento hacia adelante y hacia atrás (inverso)
     if keys[pygame.K_DOWN]:
-        triangle_speed_x = math.cos(math.radians(triangle_angle - 90)) * triangle_speed
-        triangle_speed_y = -math.sin(math.radians(triangle_angle - 90)) * triangle_speed
-        triangle_x += triangle_speed_x
-        triangle_y += triangle_speed_y
+        triangle_speed_x += math.cos(math.radians(triangle_angle - 90)) * triangle_speed
+        triangle_speed_y += -math.sin(math.radians(triangle_angle - 90)) * triangle_speed
     if keys[pygame.K_UP]:
-        triangle_speed_x = -math.cos(math.radians(triangle_angle - 90)) * triangle_speed
-        triangle_speed_y = math.sin(math.radians(triangle_angle - 90)) * triangle_speed
-        triangle_x += triangle_speed_x
-        triangle_y += triangle_speed_y
+        triangle_speed_x += -math.cos(math.radians(triangle_angle - 90)) * triangle_speed
+        triangle_speed_y += math.sin(math.radians(triangle_angle - 90)) * triangle_speed
 
     # Movimiento lateral izquierdo y derecho
     if keys[pygame.K_LEFT]:
@@ -56,11 +56,19 @@ while running:
     if keys[pygame.K_RIGHT]:
         triangle_angle -= triangle_rotation_speed
 
-    # Actualizar la inercia
-    inertia_speed_x *= inertia
-    inertia_speed_y *= inertia
-    triangle_x += inertia_speed_x
-    triangle_y += inertia_speed_y
+    # Aplicar fricción
+    triangle_speed_x *= current_friction
+    triangle_speed_y *= current_friction
+
+    # Actualizar posición
+    triangle_x += triangle_speed_x
+    triangle_y += triangle_speed_y
+
+    # Cambiar fricción según si las teclas están presionadas
+    if any(keys):
+        current_friction = slippery_friction
+    else:
+        current_friction = friction
 
     # Dibujar el fondo
     screen.fill(black)
